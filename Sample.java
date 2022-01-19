5640e6b9fb10267b5f200e12746c1be85e8fd479
DayasSaji <97952445+DayasSaji@users.noreply.github.com>
2022-01-19 17:32:23 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,12 @@
+package manu;
+
+public class Sample {
+	public void print() {
+	 System.out.println("malluuuuzzzzz");
+	}
+	 public static void main(String[] args) {
+		 Sample s= new Sample();
+		 s.print();
+		//print();
+	}
+}
